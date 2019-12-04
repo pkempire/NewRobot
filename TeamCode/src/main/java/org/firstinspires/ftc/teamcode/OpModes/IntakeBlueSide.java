5762e9de4de68a8fc274ec152adf82dba2268c0e
pkempire <31319113+pkempire@users.noreply.github.com>
2019-12-04 16:26:27 -0500

    intaking block 12/4

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/IntakeBlueSide.java
@@ -0,0 +1,196 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.Movement.Drive;
+import org.firstinspires.ftc.teamcode.Hardware.Intake;
+import org.firstinspires.ftc.teamcode.Odometry.Odometer2;
+
+import org.firstinspires.ftc.teamcode.CustomCV.MainPipeline;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+
+@Autonomous(name="Intake Blue Auto", group="Linear Opmode")
+
+public class IntakeBlueSide extends LinearOpMode {
+
+    // Declare OpMode members ======================================================================
+    private DcMotor RightFront;
+    private DcMotor RightBack;
+    private DcMotor LeftFront;
+    private DcMotor LeftBack;
+
+    private DcMotor intakeLeft;
+    private DcMotor intakeRight;
+
+    private Servo blockHook;
+
+    private Odometer2 Adham;
+    private Drive Driver;
+    private Intake Intaker;
+
+    private MainPipeline pipeline;
+    private OpenCvCamera phoneCam;
+
+    // Important Variables =========================================================================
+    private int skyPosition;
+
+    private BNO055IMU Imu;
+    private BNO055IMU.Parameters Params;
+
+    private void initialize(){
+
+        telemetry.addData("Status: ", "Initializing");
+        telemetry.update();
+
+        // Initialize all objects declared above
+        // Initialize all objects declared above
+        RightFront = hardwareMap.dcMotor.get("driveFrontRight");
+        LeftFront = hardwareMap.dcMotor.get("driveFrontLeft");
+        LeftBack = hardwareMap.dcMotor.get("driveBackLeft");
+        RightBack = hardwareMap.dcMotor.get("driveBackRight");
+
+        blockHook = hardwareMap.servo.get("blockGrabberFront");
+
+        intakeLeft = hardwareMap.dcMotor.get("intakeLeft");
+        intakeRight = hardwareMap.dcMotor.get("intakeRight");
+
+        blockHook.setPosition(0.2);
+        Params = new BNO055IMU.Parameters();
+        Params.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        Params.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        Params.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opMode
+        Params.loggingEnabled      = true;
+        Params.loggingTag          = "IMU";
+        Params.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+        Imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+        //==========================================================================================
+        Imu.initialize(Params);
+        Adham = new Odometer2(RightFront, LeftFront, LeftBack, Imu, -1, -1, -1, this);
+        Adham.initialize(0, 0, 0);
+
+        Driver = new Drive(LeftFront, RightFront, LeftBack, RightBack, Adham, this);
+        Driver.initialize();
+
+
+        // Vision
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+
+        phoneCam = new OpenCvInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+        phoneCam.openCameraDevice();
+
+        pipeline = new MainPipeline();
+        phoneCam.setPipeline(pipeline);
+        phoneCam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+
+        telemetry.addData("Status: ", "Initialized");
+        telemetry.update();
+    }
+
+    @Override
+    public void runOpMode() {
+        // Wait for the game to start (driver presses PLAY)
+        initialize();
+        waitForStart();
+
+        telemetry.addData("Status: ", "Running");
+        telemetry.update();
+        //Start Autonomous period
+        intakeBlock();
+        delay(30);
+        Driver.strafeToPointOrient(-40, -14, 0, 3, 2);
+        delay(7);
+        scanSkystone();
+        delay(7);
+
+        if(skyPosition == 0) {
+
+            delay(20);
+
+            delay(20);
+        }else if(skyPosition == 1) {
+            Driver.strafeToPointOrient(27,71,48,2.5,2);
+            delay(20);
+            Driver.strafeToPointOrient(18,89,54,3,2);
+            delay(20);
+            intakeBlock();
+        }else if(skyPosition == 2) {
+
+            delay(20);
+
+            delay(20);
+        }
+
+        delay(20);
+
+        delay(7);
+
+        if(skyPosition == 0) {
+
+            delay(20);
+
+            delay(20);
+        }else if(skyPosition == 1) {
+
+            delay(20);
+
+            delay(20);
+        }else if(skyPosition == 2) {
+
+            delay(20);
+
+            delay(20);
+        }
+
+        //park
+
+    }
+
+    private void scanSkystone(){
+        skyPosition = pipeline.getSkystonePosition();
+
+        if(skyPosition == 404) {
+            scanSkystone();
+        }
+    }
+
+    private void delay(int millis) {
+        int limit = (int)(millis/2);
+        for(int x=0;x<limit; x++) {
+            if (opModeIsActive()) {
+                Driver.localize();
+                try{Thread.sleep(2);}catch(InterruptedException e){e.printStackTrace();}
+            }else {
+                break;
+            }
+        }
+    }
+
+    private void intakeBlock(){
+        intakeLeft.setPower(-0.6);
+        intakeRight.setPower(-0.6);
+        delay(100);
+        LeftBack.setPower(0.15);
+        LeftFront.setPower(0.15);
+        RightBack.setPower(0.15);
+        RightFront.setPower(0.15);
+        delay(15);
+        LeftBack.setPower(0);
+        LeftFront.setPower(0);
+        RightBack.setPower(0);
+        RightFront.setPower(0);
+        intakeLeft.setPower(-0.6);
+        intakeRight.setPower(-0.6);
+        delay(30);
+        intakeLeft.setPower(0);
+        intakeRight.setPower(0);
+    }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/wireless.bat
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/wireless.bat
@@ -1,6 +1,6 @@
 cd C:\Users\FTC\AppData\Local\Android\sdk\platform-tools
 adb tcpip 5555
-TIMEOUT  3
+TIMEOUT  2
 adb connect 192.168.49.1
 TIMEOUT  2
 QUIT
